decf30d1b5d4c3b87c3d86df133af9ee8d389f47
bananinhamj <viliciznskimj@gmail.com>
2023-03-07 15:02:56 -0300

    aula-07-03

--- livraria/models.py
+++ livraria/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
-from django.db import models
-
 class Categoria(models.Model):
     descricao = models.CharField(max_length=100)
+    def __str__(self):
+            return "{self.descricao} ({self.id})"
+
+class Editora(models.Model):
+   nome = models.CharField(max_length=100)
+   site = models.URLField (null=True, blank=True) #esse campo é opcional
+
+   def __str__(self):
+       return self.nome
